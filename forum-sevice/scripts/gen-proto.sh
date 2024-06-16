7d6e886f69402a20752485bab7fa4a23f851428b
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-06-16 16:06:29 +0500

    15%

--- forum-sevice/scripts/gen-proto.sh
+++ forum-sevice/scripts/gen-proto.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CURRENT_DIR=$1
-rm -rf ${CURRENT_DIR}/proto-service/genprotos
-for x in $(find ${CURRENT_DIR}/proto-service/* -type d); do
-  protoc -I=${x} -I=${CURRENT_DIR}/proto-service/forum-protos -I /usr/local/go --go_out=${CURRENT_DIR} \
+rm -rf ${CURRENT_DIR}/genprotos
+for x in $(find ${CURRENT_DIR}/forum-protos -type d); do
+  echo 1
+  protoc -I=${x} -I=${CURRENT_DIR}/forum-protos -I /usr/local/go --go_out=${CURRENT_DIR} \
    --go-grpc_out=${CURRENT_DIR} ${x}/*.proto
 done
